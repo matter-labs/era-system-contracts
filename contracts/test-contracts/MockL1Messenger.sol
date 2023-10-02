9911cd25003fd5faa9956da29f5b2e731e927422
AntonD3 <antonv.dyadyuk@gmail.com>
2023-10-02 19:20:12 +0200

    Fix mock l1 messenger

--- contracts/test-contracts/MockL1Messenger.sol
+++ contracts/test-contracts/MockL1Messenger.sol
@@ -8,4 +8,9 @@ contract MockL1Messenger {
     function requestBytecodeL1Publication(bytes32 _bytecodeHash) external {
         emit MockBytecodeL1Published(_bytecodeHash);
     }
+
+    // To prevent failing during calls from the bootloader
+    function sendToL1(bytes calldata) external returns (bytes32) {
+        return bytes32(0);
+    }
 }
