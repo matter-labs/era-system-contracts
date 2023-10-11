456679846c35fd074c2876b877a04f91ab299bf2
Bence Haromi <bence.haromi@gmail.com>
2023-10-11 21:44:25 +0100

    chore: major calculate-hashes refactor

--- scripts/calculate-hashes.ts
+++ scripts/calculate-hashes.ts
@@ -1,116 +1,346 @@
 import { ethers } from "ethers";
-import { readFileSync, writeFileSync } from "fs";
+import * as fs from "fs";
+import _ from "lodash";
+import { join } from "path";
 import { hashBytecode } from "zksync-web3/build/src/utils";
+
+type ContractDetails = {
+  contractName: string;
+  sourceCodePath: string;
+  bytecodePath: string;
+};
+
+const SYSTEM_CONTRACTS_DETAILS: ContractDetails[] = [
+  // contracts dir sol
+  {
+    contractName: "AccountCodeStorage",
+    sourceCodePath: "contracts/AccountCodeStorage.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/AccountCodeStorage.sol/AccountCodeStorage.json",
+  },
+  {
+    contractName: "BootloaderUtilities",
+    sourceCodePath: "contracts/BootloaderUtilities.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/BootloaderUtilities.sol/BootloaderUtilities.json",
+  },
+  {
+    contractName: "ComplexUpgrader",
+    sourceCodePath: "contracts/ComplexUpgrader.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/ComplexUpgrader.sol/ComplexUpgrader.json",
+  },
+  {
+    contractName: "Compressor",
+    sourceCodePath: "contracts/Compressor.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/Compressor.sol/Compressor.json",
+  },
+  {
+    contractName: "ContractDeployer",
+    sourceCodePath: "contracts/ContractDeployer.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/ContractDeployer.sol/ContractDeployer.json",
+  },
+  {
+    contractName: "DefaultAccount",
+    sourceCodePath: "contracts/DefaultAccount.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/DefaultAccount.sol/DefaultAccount.json",
+  },
+  {
+    contractName: "EmptyContract",
+    sourceCodePath: "contracts/EmptyContract.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/EmptyContract.sol/EmptyContract.json",
+  },
+  {
+    contractName: "ImmutableSimulator",
+    sourceCodePath: "contracts/ImmutableSimulator.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/ImmutableSimulator.sol/ImmutableSimulator.json",
+  },
+  {
+    contractName: "KnownCodesStorage",
+    sourceCodePath: "contracts/KnownCodesStorage.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/KnownCodesStorage.sol/KnownCodesStorage.json",
+  },
+  {
+    contractName: "L1Messenger",
+    sourceCodePath: "contracts/L1Messenger.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/L1Messenger.sol/L1Messenger.json",
+  },
+  {
+    contractName: "L2EthToken",
+    sourceCodePath: "contracts/L2EthToken.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/L2EthToken.sol/L2EthToken.json",
+  },
+  {
+    contractName: "MsgValueSimulator",
+    sourceCodePath: "contracts/MsgValueSimulator.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/MsgValueSimulator.sol/MsgValueSimulator.json",
+  },
+  {
+    contractName: "NonceHolder",
+    sourceCodePath: "contracts/NonceHolder.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/NonceHolder.sol/NonceHolder.json",
+  },
+  {
+    contractName: "SystemContext",
+    sourceCodePath: "contracts/SystemContext.sol",
+    bytecodePath:
+      "artifacts-zk/cache-zk/solpp-generated-contracts/SystemContext.sol/SystemContext.json",
+  },
+  // contracts dir yul
+  {
+    contractName: "EventWriter",
+    sourceCodePath: "contracts/EventWriter.yul",
+    bytecodePath: "contracts/artifacts/EventWriter.yul/EventWriter.yul.zbin",
+  },
+  // precompiles dir yul
+  {
+    contractName: "EcAdd",
+    sourceCodePath: "contracts/precompiles/EcAdd.yul",
+    bytecodePath: "contracts/precompiles/artifacts/EcAdd.yul/EcAdd.yul.zbin",
+  },
+  {
+    contractName: "EcMul",
+    sourceCodePath: "contracts/precompiles/EcMul.yul",
+    bytecodePath: "contracts/precompiles/artifacts/EcMul.yul/EcMul.yul.zbin",
+  },
+  {
+    contractName: "Ecrecover",
+    sourceCodePath: "contracts/precompiles/Ecrecover.yul",
+    bytecodePath:
+      "contracts/precompiles/artifacts/Ecrecover.yul/Ecrecover.yul.zbin",
+  },
+  {
+    contractName: "Keccak256",
+    sourceCodePath: "contracts/precompiles/Keccak256.yul",
+    bytecodePath:
+      "contracts/precompiles/artifacts/Keccak256.yul/Keccak256.yul.zbin",
+  },
+  {
+    contractName: "Sha256",
+    sourceCodePath: "contracts/precompiles/Sha256.yul",
+    bytecodePath: "contracts/precompiles/artifacts/Sha256.yul/Sha256.yul.zbin",
+  },
+  // bootloader dir yul
+  {
+    contractName: "proved_batch",
+    sourceCodePath: "bootloader/build/proved_batch.yul",
+    bytecodePath:
+      "bootloader/build/artifacts/proved_batch.yul/proved_batch.yul.zbin",
+  },
+];
 
 type Hashes = {
   sourceCodeHash: string;
   bytecodeHash: string;
 };
 
+type SystemContractHashes = ContractDetails & Hashes;
+
 type SystemContractsHashes = {
-  bootloader: Hashes;
-  defaultAA: Hashes;
+  [key: string]: SystemContractHashes;
 };
 
-const BOOTLOADER_SOURCE_CODE_PATH = "./bootloader/build/proved_batch.yul";
-const BOOTLOADER_BYTECODE_PATH =
-  "./bootloader/build/artifacts/proved_batch.yul/proved_batch.yul.zbin";
-const DEFAULT_ACCOUNT_SOURCE_CODE_PATH = "./contracts/DefaultAccount.sol";
-const DEFAULT_ACCOUNT_JSON_PATH =
-  "./artifacts-zk/cache-zk/solpp-generated-contracts/DefaultAccount.sol/DefaultAccount.json";
-const OUTPUT_FILE_PATH = "./SystemContractsHashes.json";
+const makePathAbsolute = (path: string): string => {
+  return join(__dirname, "..", path);
+};
 
-const readFileAsHexString = (path: string, errorMessage: string): string => {
+const readSourceCode = (details: ContractDetails): string => {
   try {
-    return "0x" + readFileSync(path, "hex");
-  } catch {
-    throw new Error(errorMessage);
+    return ethers.utils.hexlify(fs.readFileSync(details.sourceCodePath));
+  } catch (err) {
+    const msg = err instanceof Error ? err.message : "Unknown error";
+    throw new Error(
+      `Failed to read source code for ${details.contractName}: ${details.sourceCodePath} Error: ${msg}`
+    );
   }
 };
 
-const loadBytecodeFromJson = (path: string, errorMessage: string): string => {
+const readBytecode = (details: ContractDetails): string => {
   try {
-    const jsonFile = readFileSync(path, "utf8");
-    return JSON.parse(jsonFile).bytecode;
-  } catch {
-    throw new Error(errorMessage);
+    if (details.bytecodePath.endsWith(".json")) {
+      const jsonFile = fs.readFileSync(details.bytecodePath, "utf8");
+      return ethers.utils.hexlify(JSON.parse(jsonFile).bytecode);
+    } else {
+      return ethers.utils.hexlify(fs.readFileSync(details.bytecodePath));
+    }
+  } catch (err) {
+    const msg = err instanceof Error ? err.message : "Unknown error";
+    throw new Error(
+      `Failed to read bytecode for ${details.contractName}: ${details.bytecodePath} Error: ${msg}`
+    );
   }
 };
 
 const getHashes = (
+  contractName: string,
   sourceCode: string,
-  bytecode: string,
-  errorMessage: string
+  bytecode: string
 ): Hashes => {
   try {
     return {
-      sourceCodeHash: ethers.utils.sha256(sourceCode),
       bytecodeHash: ethers.utils.hexlify(hashBytecode(bytecode)),
+      // The extra checks performed by the hashBytecode function are not needed for the source code, therefore
+      // sha256 is used for simplicity
+      sourceCodeHash: ethers.utils.sha256(sourceCode),
     };
-  } catch {
-    throw new Error(errorMessage);
+  } catch (err) {
+    const msg = err instanceof Error ? err.message : "Unknown error";
+    throw new Error(
+      `Failed to calculate hashes for ${contractName} Error: ${msg}`
+    );
   }
 };
 
+const withLowercaseFirstLetter = (name: string) => {
+  return name.charAt(0).toLowerCase() + name.slice(1);
+};
+
+const getSystemContractsHashes = (
+  systemContractsDetails: ContractDetails[]
+): SystemContractsHashes =>
+  systemContractsDetails.reduce((systemContractsHashes, contractDetails) => {
+    const sourceCode = readSourceCode(contractDetails);
+    const bytecode = readBytecode(contractDetails);
+    const hashes = getHashes(
+      contractDetails.contractName,
+      sourceCode,
+      bytecode
+    );
+
+    const systemContractHashes: SystemContractHashes = {
+      ...contractDetails,
+      ...hashes,
+    };
+
+    const keyName = withLowercaseFirstLetter(contractDetails.contractName);
+
+    return {
+      ...systemContractsHashes,
+      [keyName]: systemContractHashes,
+    };
+  }, {});
+
+const OUTPUT_FILE_PATH = "SystemContractsHashes.json";
+
+const readSystemContractsHashesFile = (path: string): SystemContractsHashes => {
+  const absolutePath = makePathAbsolute(path);
+  try {
+    const file = fs.readFileSync(absolutePath, "utf8");
+    const parsedFile = JSON.parse(file);
+    return parsedFile;
+  } catch (err) {
+    const msg = err instanceof Error ? err.message : "Unknown error";
+    throw new Error(`Failed to read file: ${absolutePath} Error: ${msg}`);
+  }
+};
+
+const saveSystemContractsHashesFile = (
+  path: string,
+  systemContractsHashes: SystemContractsHashes
+) => {
+  const absolutePath = makePathAbsolute(path);
+  try {
+    fs.writeFileSync(
+      absolutePath,
+      JSON.stringify(systemContractsHashes, null, 2)
+    );
+  } catch (err) {
+    const msg = err instanceof Error ? err.message : "Unknown error";
+    throw new Error(`Failed to save file: ${absolutePath} Error: ${msg}`);
+  }
+};
+
+const findDifferences = (
+  newHashes: SystemContractsHashes,
+  oldHashes: SystemContractsHashes
+) => {
+  const differences = _.xorWith(
+    Object.entries(newHashes),
+    Object.entries(oldHashes),
+    _.isEqual
+  );
+
+  const differencesUniqueKeys = _.uniq(differences.map(([key]) => key));
+
+  const differencesList = differencesUniqueKeys.map((key) => {
+    const differingFields = _.xorWith(
+      Object.entries(newHashes[key] || {}),
+      Object.entries(oldHashes[key] || {}),
+      _.isEqual
+    );
+
+    const differingFieldsUniqueKeys = _.uniq(
+      differingFields.map(([key]) => key)
+    );
+
+    return {
+      key: key,
+      differingFields: differingFieldsUniqueKeys,
+      old: oldHashes[key] || {},
+      new: newHashes[key] || {},
+    };
+  });
+
+  return differencesList;
+};
+
 const main = async () => {
-  const checkOnly = process.argv.includes("--check-only");
+  const args = process.argv;
+  if (args.length > 3 || (args.length == 3 && !args.includes("--check-only"))) {
+    console.log(
+      "This command can be used with no arguments or with the --check-only flag. Use the --check-only flag to check the hashes without updating the SystemContractsHashes.json file."
+    );
+    process.exit(1);
+  }
+  const checkOnly = args.includes("--check-only");
 
-  const bootloaderSourceCode = readFileAsHexString(
-    BOOTLOADER_SOURCE_CODE_PATH,
-    "Failed to read Bootloader source code. Make sure to run `yarn build-yul` before you run this script!"
-  );
-  const bootloaderBytecode = readFileAsHexString(
-    BOOTLOADER_BYTECODE_PATH,
-    "Failed to read Bootloader bytecode. Make sure to run `yarn build-yul` before you run this script!"
-  );
-  const defaultAASourceCode = readFileAsHexString(
-    DEFAULT_ACCOUNT_SOURCE_CODE_PATH,
-    "Failed to read DefaultAccount source code. Make sure to run `yarn build` before you run this script!"
-  );
-  const defaultAABytecode = loadBytecodeFromJson(
-    DEFAULT_ACCOUNT_JSON_PATH,
-    "Failed to read DefaultAccount bytecode. Make sure to run `yarn build` before you run this script!"
+  const newSystemContractsHashes = getSystemContractsHashes(
+    SYSTEM_CONTRACTS_DETAILS
   );
 
-  const systemContractsHashes: SystemContractsHashes = {
-    bootloader: getHashes(
-      bootloaderSourceCode,
-      bootloaderBytecode,
-      "Failed to calculate Bootloader hashes."
-    ),
-    defaultAA: getHashes(
-      defaultAASourceCode,
-      defaultAABytecode,
-      "Failed to calculate DefaultAccount hashes."
-    ),
-  };
+  const oldSystemContractsHashes =
+    readSystemContractsHashesFile(OUTPUT_FILE_PATH);
 
-  const newSystemContractsHashes = JSON.stringify(
-    systemContractsHashes,
-    null,
-    2
-  );
-
-  const oldSystemContractsHashes = readFileSync(OUTPUT_FILE_PATH, "utf8");
-
-  if (oldSystemContractsHashes === newSystemContractsHashes) {
+  if (_.isEqual(newSystemContractsHashes, oldSystemContractsHashes)) {
     console.log(
       "Calculated hashes match the hashes in the SystemContractsHashes.json file."
     );
+    console.log("Exiting...");
     return;
-  } else if (checkOnly) {
-    console.error(
-      "Calculated hashes differ from the hashes in the SystemContractsHashes.json file. Exiting..."
+  }
+
+  const differences = findDifferences(
+    newSystemContractsHashes,
+    oldSystemContractsHashes
+  );
+
+  console.log(
+    "Calculated hashes differ from the hashes in the SystemContractsHashes.json file. Differences:"
+  );
+  console.log(differences);
+
+  if (checkOnly) {
+    console.log(
+      "You can use the `yarn calculate-hashes` command to update the SystemContractsHashes.json file."
     );
+    console.log("Exiting...");
     process.exit(1);
   } else {
-    console.log(
-      "Calculated hashes differ from the hashes in the SystemContractsHashes.json file. Updating..."
-    );
-
-    writeFileSync(OUTPUT_FILE_PATH, newSystemContractsHashes);
-    console.log("Update finished. New hashes:");
-    console.log(newSystemContractsHashes);
+    console.log("Updating...");
+    saveSystemContractsHashesFile(OUTPUT_FILE_PATH, newSystemContractsHashes);
+    console.log("Update finished");
+    console.log("Exiting...");
+    return;
   }
 };
 
@@ -118,5 +348,8 @@ main()
   .then(() => process.exit(0))
   .catch((err) => {
     console.error("Error:", err.message || err);
+    console.log(
+      "Please make sure to run `yarn build && yarn preprocess && yarn compile-yul` before running this script."
+    );
     process.exit(1);
   });
